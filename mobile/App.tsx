336152023c341b4671222505956edd272845ea29
Carlos H Kruger <carloshkruger@gmail.com>
2020-12-25 13:23:25 -0300

    finalizado primeira versao

---
+++ mobile/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+import { useFonts } from 'expo-font';
+import {
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+  Nunito_800ExtraBold,
+} from '@expo-google-fonts/nunito';
+import Routes from './src/routes';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <StatusBar
+        backgroundColor="transparent"
+        translucent
+        barStyle="dark-content"
+      />
+      <Routes />
+    </>
+  );
+}
